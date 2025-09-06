4900458026bae4e108657b118f361ca62e0feb59
shivamghaware <shivamghawre3@gmail.com>
2025-09-06 15:28:58 +0530

    Initial commit

---
+++ organic/src/main/java/com/ecom/organic/OrganicApplication.java
@@ -0,0 +1,13 @@
+package com.ecom.organic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrganicApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OrganicApplication.class, args);
+	}
+
+}
---
+++ organic/src/main/java/com/ecom/organic/controller/ProductController.java
@@ -0,0 +1,101 @@
+package com.ecom.organic.controller;
+
+import com.ecom.organic.model.Product;
+import com.ecom.organic.service.ProductService;
+import org.apache.tomcat.util.file.ConfigurationSource;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+import java.util.List;
+
+@RestController
+@RequestMapping("/api")
+@CrossOrigin
+public class ProductController {
+
+    @Autowired
+    private ProductService service;
+
+    @GetMapping("/")
+    public String greet(){
+        return "Hello";
+    }
+
+    @GetMapping("/products")
+    public ResponseEntity<List<Product>>  getAllProducts(){
+        return new ResponseEntity<>(service.getAllProducts(),HttpStatus.OK);
+    }
+
+    @GetMapping("/product/{id}")
+    public ResponseEntity<Product> getProductById(@PathVariable int id){
+        Product prod=service.getProductById(id);
+        if(prod!=null){
+            return new ResponseEntity<>(prod,HttpStatus.OK);
+        }else{
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+    }
+
+    @PostMapping("/product")
+    public ResponseEntity<?> addProduct(@RequestPart Product product, @RequestPart MultipartFile imageFile){
+        try{
+            Product prod1=service.addProduct(product,imageFile);
+            return new ResponseEntity<>(prod1,HttpStatus.CREATED);
+        }
+        catch(Exception e){
+            return new ResponseEntity<>(e.getMessage(),HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+
+    @GetMapping("/product/{prodId}/image")
+    public ResponseEntity<byte[]> getImageByProductID(@PathVariable int prodId){
+        Product prod=service.getProductById(prodId);
+        byte[] imageFile=prod.getImageData();
+//        return new ResponseEntity<>(imageFile,HttpStatus.OK);
+
+        return ResponseEntity.ok().
+                contentType(MediaType.valueOf(prod.getImageType()))
+                .body(imageFile);
+    }
+
+    @PutMapping("/product/{prodId}")
+    public ResponseEntity<String> updateProduct(@PathVariable int prodId,
+                                                @RequestPart Product product,
+                                                @RequestPart MultipartFile imageFile){
+        Product prod1= null;
+        try {
+            prod1 = service.updateProduct(prodId,product,imageFile);
+        } catch (IOException e) {
+            return new ResponseEntity<>("Failed to Update",HttpStatus.BAD_REQUEST);
+        }
+        if(prod1!=null){
+            return new ResponseEntity<>("Updated",HttpStatus.OK);
+        }else{
+            return new ResponseEntity<>("Failed to Update",HttpStatus.BAD_REQUEST);
+        }
+    }
+
+    @DeleteMapping("/product/{id}")
+    public ResponseEntity<String> deleteProduct(@PathVariable int id){
+        Product product=service.getProductById(id);
+        if(product!=null){
+            service.deleteProduct(id);
+            return new ResponseEntity<>("Deleted",HttpStatus.OK);
+        }else{
+            return new ResponseEntity<>("Product not Found",HttpStatus.NOT_FOUND);
+        }
+    }
+
+    @GetMapping("/products/search")
+    public ResponseEntity<List<Product>> searchProducts(@RequestParam String keyword){
+        System.out.println("Searching With "+keyword);
+        List<Product> product=service.searchProducts( keyword);
+        return new ResponseEntity<>(product,HttpStatus.OK);
+    }
+
+}
