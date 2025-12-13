3227d2e62a9aa261e88c675d5a3eb8ad509f4553
shivamghaware <shivamghawre3@gmail.com>
2025-12-13 11:25:33 +0530

    feat: Introduce Spring Boot application configuration for PostgreSQL and multipart uploads upto 10mb, and add comprehensive unit tests for the ProductController.

--- organic/src/test/java/com/ecom/organic/controller/ProductControllerTest.java
+++ organic/src/test/java/com/ecom/organic/controller/ProductControllerTest.java
@@ -6,12 +6,10 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.http.MediaType;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.mock.web.MockMultipartFile;
 import org.springframework.test.web.servlet.MockMvc;
 
-import java.math.BigDecimal;
 import java.util.Arrays;
 import java.util.List;
 
@@ -28,7 +26,7 @@ class ProductControllerTest {
     @Autowired
     private MockMvc mockMvc;
 
-    @MockBean
+    @MockitoBean
     private ProductService service;
 
     @Autowired
